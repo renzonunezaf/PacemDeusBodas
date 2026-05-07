7e64da556f4bf843a978a2c42f057445f1896584
AngelinaMRCh <165961478+AngelinaMRCh@users.noreply.github.com>
2026-05-07 13:25:27 -0500

    Update OfflineSetlistViewModel.kt

--- PacemDeusBodas/app/src/main/java/com/pacemdeus/bodas/ui/couple/OfflineSetlistViewModel.kt
+++ PacemDeusBodas/app/src/main/java/com/pacemdeus/bodas/ui/couple/OfflineSetlistViewModel.kt
@@ -1,1 +1,21 @@
+class OfflineSetlistViewModel : ViewModel() {
 
+    var songs by mutableStateOf<List<SetlistSong>>(emptyList())
+
+    fun downloadSetlist(weddingId: Int) {
+
+        viewModelScope.launch {
+
+            try {
+
+                val response =
+                    ApiClient.apiService.getOfflineSetlist(weddingId)
+
+                songs = response.items
+
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+}
