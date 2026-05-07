7d321e416946d491c5d0606695e3af6086d97ba7
AngelinaMRCh <165961478+AngelinaMRCh@users.noreply.github.com>
2026-05-07 15:48:19 -0500

    Update SetlistDao.kt

--- PacemDeusBodas/app/src/main/java/com/pacemdeus/bodas/data/local/dao/SetlistDao.kt
+++ PacemDeusBodas/app/src/main/java/com/pacemdeus/bodas/data/local/dao/SetlistDao.kt
@@ -1,1 +1,11 @@
+@Dao
+interface SetlistDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(
+        items: List<SetlistEntity>
+    )
+
+    @Query("SELECT * FROM offline_setlist")
+    suspend fun getAll(): List<SetlistEntity>
+}
